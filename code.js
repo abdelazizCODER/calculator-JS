10ec772a418dae2962cbefaf442309ee1ef75a74
abdelazizCODER <159784196+abdelazizCODER@users.noreply.github.com>
2024-03-02 09:22:16 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,66 @@
+document.querySelector("#n1").onclick = function(){
+    document.querySelector(".text").value += 1;
+}
+document.querySelector("#n2").onclick = function(){
+    document.querySelector(".text").value += 2;
+}
+document.querySelector("#n3").onclick = function(){
+    document.querySelector(".text").value += 3;
+}
+document.querySelector("#n4").onclick = function(){
+    document.querySelector(".text").value += 4;
+}
+document.querySelector("#n5").onclick = function(){
+    document.querySelector(".text").value += 5;
+}
+document.querySelector("#n6").onclick = function(){
+    document.querySelector(".text").value += 6;
+}
+document.querySelector("#n7").onclick = function(){
+    document.querySelector(".text").value += 7;
+}
+document.querySelector("#n8").onclick = function(){
+    document.querySelector(".text").value += 8;
+}
+document.querySelector("#n9").onclick = function(){
+    document.querySelector(".text").value += 9;
+}
+document.querySelector("#nc").onclick = function(){
+    document.querySelector(".text").value += 'c';
+}
+document.querySelector("#number1").onclick = function(){
+    document.querySelector(".text").value += '/';
+}
+document.querySelector("#number2").onclick = function(){
+    document.querySelector(".text").value += '%';
+}
+document.querySelector("#number3").onclick = function(){
+    document.querySelector(".text").value += '*';
+}
+document.querySelector("#number4").onclick = function(){
+    document.querySelector(".text").value += '/';
+}
+document.querySelector("#number5").onclick = function(){
+    document.querySelector(".text").value += '-';
+}
+document.querySelector("#number6").onclick = function(){
+    document.querySelector(".text").value += '+';
+}
+document.querySelector("#number7").onclick = function(){
+    document.querySelector(".text").value += '.';
+}
+document.querySelector("#number8").onclick = function(){
+    document.querySelector(".text").value += '=';
+}
+document.querySelector("#n0").onclick = function(){
+    document.querySelector(".text").value += 0;
+}
+document.querySelector("#nc").onclick = function(){
+    document.querySelector(".text").value = '';
+}
+document.querySelector("#number8").onclick = function(){
+    document.querySelector(".text").value = 
+    eval(document.querySelector('.text').value)
+}
+
+  
